4488ba1e02bf3e74b0acf02af7042be163cad11e
Kanchana Dhana Sadasivan <30665819+Kanchi11@users.noreply.github.com>
2025-02-03 17:20:24 -0500

    Update rand.py

--- rand.py
+++ rand.py
@@ -6,6 +6,6 @@ import random
 
 def random_array(arr):
     """Generates a random array with values between 1 and 20."""
-    for i in range(len(arr)):
+    for i, _ in enumerate(arr):
         arr[i] = random.randint(1, 20)  # Replaced subprocess with random.randint()
     return arr
