3c04fb3a1c653178b08e3a91a574928b4c932572
Brandon Troy <55519057+BrandonTroy@users.noreply.github.com>
2025-02-05 08:33:04 +0000

    Fixed more static analysis issues

--- rand.py
+++ rand.py
@@ -1,8 +1,11 @@
+"""This module provides a function to generate a random array of integers."""
+
 import subprocess
 
-def random_array(arr):
-    shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
-        arr[i] = int(shuffled_num.stdout)
+def random_array(length):
+    """Generates a random array of integers."""
+    arr = []
+    for _ in range(length):
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
+        arr.append(int(shuffled_num.stdout))
     return arr
